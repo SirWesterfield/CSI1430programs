dbaa5ac3d26dab3043c8f49d174c0a7e5c2dc76e
SirWesterfield <35206122+SirWesterfield@users.noreply.github.com>
2022-03-28 13:26:51 -0500

    Program 19 finished

--- Program 19/arrayCount.cpp
+++ Program 19/arrayCount.cpp
@@ -7,3 +7,112 @@
 
 #include "arrayCount.h"
 
+
+int arrayCount(int a[], int s, int v)
+{
+    int frequency = 0;
+    for (int i = 0; i < s; i++)
+    {
+        if (a[i] == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(double a[], int s, double v)
+{
+    int frequency = 0;
+    for (int i = 0; i < s; i++)
+    {
+        if (a[i] == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(string a[], int s, string v)
+{
+    int frequency = 0;
+    for (int i = 0; i < s; i++)
+    {
+        if (a[i] == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(char a[], char v)
+{
+    int frequency = 0;
+    int cstringSize = strlen(a);
+    for (int i = 0; i < cstringSize; i++)
+    {
+        if (a[i] == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(vector<int> a, int v)
+{
+    int frequency = 0;
+    int vectorSize = a.size();
+    for (int i = 0; i < vectorSize; i++)
+    {
+        if (a.at(i) == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(vector<string> a, string v)
+{
+    int frequency = 0;
+    int vectorSize = a.size();
+    for (int i = 0; i < vectorSize; i++)
+    {
+        if (a.at(i) == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(vector<double> a, double v)
+{
+    int frequency = 0;
+    int vectorSize = a.size();
+    for (int i = 0; i < vectorSize; i++)
+    {
+        if (a.at(i) == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
+
+int arrayCount(vector<char> a, char v)
+{
+    int frequency = 0;
+    int vectorSize = a.size();
+    for (int i = 0; i < vectorSize; i++)
+    {
+        if (a.at(i) == v)
+        {
+            frequency++;
+        }
+    }
+    return frequency;
+}
--- Program 19/arrayCount.h
+++ Program 19/arrayCount.h
@@ -10,13 +10,14 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 /*
  * description: takes an array of ints, an integer for size (s)
                 and a integer value (v). Finds the frequency of
                 value v in the array a.
- * return: the frequency of the value v in the array a
+ * return: integer of the frequency of the value v in the array a
  * precondition: array int a[], int s, and int v exist
  * postcondition: none of the original values are changed,
  *                and the frequency of the value of v has been returned
@@ -29,7 +30,7 @@ int arrayCount(int a[], int s, int v);
  * description: takes array of doubles, integer for size,
                 and double for specified value. Finds the frequency of V
                 in the array and returns that number.
- * return: the frequency of value v in the array a
+ * return: integer of the frequency of value v in the array a
  * precondition: array of doubles (a) exsists, integer s for size exists
  *               double v for value exsists
  * postcondition: all input values are unchanged, and the frequency of
@@ -42,59 +43,71 @@ int arrayCount(double a[], int s, double v);
 /*
  * description: takes array of strings (a), integer size (s)
                 and string value (v). Returns the frequency of v in array a
- * return: the frequency of the value v in the array a
+ * return: integer of the frequency of the value v in the array a
  * precondition: array of strings a exsits, size of array a s exsits,
  *               string v exsists
- * postcondition: array a, int s, and string v are unchange, and the fre
+ * postcondition: array a, int s, and string v are unchange, and the frequency
+ *                of the value v in the array a is returned
  *
 */
 int arrayCount(string a[], int s , string v);
 
 /*
- * description:
- * return:
- * precondition:
- * postcondition:
+ * description: takes a cstring and a char value v and returns the frequency 
+                of v in cstring a
+ * return: integer of the frequency of value v in the cstring a
+ * precondition: cstring a and char v exist
+ * postcondition: cstring a and char v have not been changed, and 
+ *                a integer indicating the frequency of v in a 
+ *                has been returned
  *
 */
 
 int arrayCount(char a[], char v);
 
 /*
- * description:
- * return:
- * precondition:
- * postcondition:
+ * description: takes vector of ints a, and int value v, and returns 
+                the frequency of value v as an integer
+ * return: integer of the frequency of value v
+ * precondition: vector of ints a and integer v exists
+ * postcondition: vector a and integer v have not been changed, and integer of
+ *                the frequency of value v has been returned
  *
 */
 
 int arrayCount(vector<int> a, int v);
 
 /*
- * description:
- * return:
- * precondition:
- * postcondition:
+ * description: takes vector of strings a, and string value v, and returns 
+                the frequency of value v as an integer
+ * return: integer of the frequency of value v
+ * precondition: vector of strings a and string v exists
+ * postcondition: vector a and string v have not been changed, and integer of
+ *                the frequency of value v has been returned
  *
 */
 
 int arrayCount(vector<string> a, string v);
 
 /*
- * description:
- * return:
- * precondition:
- * postcondition:
+ * description: takes vector of doubles a, and double value v, and returns 
+                the frequency of value v as an integer
+ * return: integer of the frequency of value v
+ * precondition: vector of doubles a and double v exists
+ * postcondition: vector a and double v have not been changed, and integer of
+ *                the frequency of value v has been returned
  *
 */
 
 int arrayCount(vector<double> a, double v);
 
 /*
- * description:
- * return:
- * precondition:
- * postcondition:
+ * description: takes vector of chars a, and char value v, and returns 
+                the frequency of value v as an integer
+ * return: integer of the frequency of value v
+ * precondition: vector of chars a and char v exists
+ * postcondition: vector a and char v have not been changed, and integer of
+ *                the frequency of value v has been returned
  *
 */
 
