8b68ed2b1a7579a6fbb7f82a70e88e045297819c
SirWesterfield <35206122+SirWesterfield@users.noreply.github.com>
2022-04-01 11:16:38 -0500

    New file for group project

---
+++ SDL tests/constants.h
@@ -0,0 +1,30 @@
+//Author: Wesley Anastasi
+//Assignment Title: 
+//Assignment Description: 
+//Due Date: 
+//Date Created: 
+//Date Last Modified: 
+
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+const int NUM_ROW = 750;
+const int NUM_COL = 500;
+
+//Colors
+
+const color_t = RED(255,20,20);
+
+
+
+/*
+ * description:
+ * return:
+ * precondition:
+ * postcondition:
+ *
+*/
+
+
+
+#endif // HEADERFILENAME_H_INCLUDED
