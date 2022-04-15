73d7dd0a73848228182634bb917e86c699797d14
Wesley Anastasi <anastasicrl@gmail.com>
2022-04-14 20:29:17 -0500

    Added a class template

---
+++ Templates and Tests/classTemplate.h
@@ -0,0 +1,32 @@
+//Author: Wesley Anastasi
+//Assignment Title: 
+//Assignment Description: 
+//Due Date: 
+//Date Created: 
+//Date Last Modified: 
+
+#ifndef CLASSTEMPLATE_H_INCLUDED
+#define CLASSTEMPLATE_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+
+class classTemplate
+{
+    public:
+    private:
+
+    /*
+    * description:
+    * return:
+    * precondition:
+    * postcondition:
+    *
+    */
+
+    classTemplate();
+};
+
+
+#endif // CLASSTEMPLATE_H_INCLUDED
