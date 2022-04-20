1ca3652b5dce2fb302e3f235c7329923293b6f6f
SirWesterfield <35206122+SirWesterfield@users.noreply.github.com>
2022-04-20 10:54:03 -0500

    started program 26

---
+++ Program 26/main.cpp
@@ -0,0 +1,42 @@
+//Author: Wesley Anastasi
+//Assignment Title: 
+//Assignment Description: 
+//Due Date: 
+//Date Created: 
+//Date Last Modified: 
+
+
+
+/*
+Data Abstraction:
+Input: 
+Process:
+Output: 
+Assumptions:
+*/
+
+
+double getCoef(string);
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Data Abstraction:
+
+    //Input:
+
+    //Process:
+
+    //Output:
+    
+
+    return 0;
+}
+
+double getCoef(string token)
+{
+    
+}
